0896afcf04f10a2ec83dafe3d01df2846dd02a13
mariaeas <dudaa2504@gmail.com>
2024-04-25 21:19:21 -0300

    aula 25/04

--- src/components/MyFirstComponent.vue
+++ src/components/MyFirstComponent.vue
@@ -1,37 +1,65 @@
 <template>
     <div class="backdrop-modal" v-if="showModal">
-<div class="modal">
-    <span @click="close()" class="close-x">x</span>
-    <h2>{{ modalTitle }}</h2>
-    <slot></slot>
-</div>
+        <div class="modal">
+            <span @click="closeModal()" class="close-x">x</span>
+            <h2>{{ modalTitle }}</h2>
+            <label>Nome do Produto</label>
+            <input type="text" v-model="productName">
+            <label>Valor do produto</label>
+            <input type="number" v-model="productValue">
+            <button @click="createProduct">Criar produto!</button>
+            <slot></slot>
+        </div>
     </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-props: {
-    modalTitle: {
-        type: String,
-        default: ''
+    data() {
+        return {
+            productName: '',
+            productValue: 0,
+        }
     },
-    showModal: {
-        type: Boolean,
-        required: true,
-    }
-},
-methods: {
-    close() {
-        this.$emit('closeModal')
-        this.$emit('update:showModal', false)
-    }
-},
+
+    props: {
+        modalTitle: {
+            type: String,
+            default: ''
+        },
+        showModal: {
+            type: Boolean,
+            required: true,
+        }
+    },
+
+    methods: {
+        createProduct() {
+            let data = {
+                name: this.productName,
+                amount: this.productValue
+            }
+            axios.post('product', data)
+                .then((response) => {
+                    console.log(response)
+                })
+                .catch((error) => {
+                    console.log(error)
+                })
+        },
+
+        closeModal() {
+   
+            this.$emit('update:showModal', false)
+        }
+    },
 }
 
 </script>
 
 <style scoped>
-.backdrop-modal{
+.backdrop-modal {
     position: fixed;
     top: 0;
     left: 0;
@@ -44,21 +72,47 @@ methods: {
     justify-content: center;
 }
 
-.modal{
-width: 400px;
-height: 250px;
-padding: 30px;
-position: relative;
-background-color: white;
-border-radius: 15px;
-color: black;
+.modal {
+    display: grid;
+    gap: 15px;
+    width: 400px;
+    height: fit-content;
+    min-height: 200px;
+    padding: 25px;
+    position: relative;
+    background-color: white;
+    border-radius: 15px;
+    color: black;
 }
 
-.close-x{
-position: absolute;
-top: 15px;
-right: 15px;
-color: black;
-cursor: pointer;
+.modal>input {
+    border: 1px solid #cdcdcd;
+    border-radius: 10px;
+    padding: 12px;
+    font-size: 16px;
+}
+
+.modal>label {
+    font-size: 16px;
+}
+
+.modal>button {
+    height: 50px;
+    background-color: #cdcdcd;
+    border: none;
+    border-radius: 10px;
+}
+
+.modal>button:hover {
+    background-color: #bdbdbd;
+
+}
+
+.close-x {
+    position: absolute;
+    top: 15px;
+    right: 15px;
+    color: black;
+    cursor: pointer;
 }
 </style>
--- src/views/Teste.vue
+++ src/views/Teste.vue
@@ -1,19 +1,62 @@
 <script>
 import Modal from '../components/MyFirstComponent.vue'
+import axios from 'axios'
+
 
 export default {
     data() {
         return {
+            selectedProduct: {},
             modalTitle: 'Meu Texto',
-            showModal: true,
+            showModal: false,
+            route: this.$route,
+            products: [],
         }
     },
 
     components: { Modal },
 
+    mounted() {
+        axios.defaults.baseURL = 'http://192.168.0.134:8000/api/'
+        this.getProducts()
+    },
+
     methods: {
+        deleteProduct() {
+            axios.delete(`product/${this.selectedProduct.id}`)
+                .then((response) => {
+                    console.log(response)
+                    let itemIndex = this.products.findIndex(product => product.id === this.selectedProduct.id)
+                    if (itemIndex) {
+                        this.products.splice(itemIndex, 1)
+                    }
+                })
+                .catch((error) => {
+                    console.log(error)
+                })
+
+        },
+
         closeModal() {
             console.log('O modal está fechado');
+        },
+
+        redirectHome() {
+            this.$router.push('/')
+
+        },
+
+        getProducts() {
+            axios.get('product')
+                .then((response) => {
+                    this.products = response.data.data
+                    console.log(this.products)
+                })
+                .catch((error) => {
+                    console.log(error)
+                })
+
+
         }
     },
 }
@@ -21,14 +64,23 @@ export default {
 </script>
 
 <template>
-    <Modal v-model:showModal='showModal' :modalTitle="'Meu texto!'" @closeModal="closeModal">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas neque reprehenderit beatae deserunt sequi
-            facilis vel natus ea autem voluptate. Ducimus laborum labore nemo odio? Commodi iusto ipsam illo odio?</p>
+    <div v-for="product in products" :key="product.id" class="product-box" @click="selectedProduct = product"
+        :class="selectedProduct.id === product.id ? 'selected-box' : ''">Item!
+        <h3>{{ product.name }}</h3>
+        <p>{{ product.amount }}</p>
+    </div>
+
+    <Modal v-model:showModal='showModal' :modalTitle="'Meu texto!'" @closeModal="closeModal" :product="selectedProduct">
     </Modal>
     <div class="about">
         <h1>Teste!</h1>
-        <button @click="showModal = true">Mostrar Modal</button>
+        <button @click="selectedProduct = {}, showModal = true">Mostrar Modal</button>
+        <button @click="selectedProduct = {}, showModal = true">Criar produto</button>
+        <button @click="showModal = true" :disabled="!selectedProduct.id">Editar produto</button>
+        <button @click="deleteProduct()">Deletar produto {{ selectedProduct.name }}</button>
     </div>
+
+
 </template>
 
 <style>
@@ -38,5 +90,21 @@ export default {
         display: flex;
         align-items: center;
     }
+}
+
+.selected-box {
+    background-color: pink;
+}
+
+.product-box {
+    transition: .3s;
+    border: 1px solid pink;
+    padding: 20px;
+    margin: 15px;
+    border-radius: 10px;
+}
+
+.product-box h3 {
+    font-size: 21px;
 }
 </style>
